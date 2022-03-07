d3cb1f1fa625895257e4bf898121b10ab73bc58e
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-03-07 14:57:41 +0100

    feat/adding first exercises

---
+++ javascript/esercizio-1/exercise-1.js
@@ -0,0 +1,15 @@
+function canPlay() {
+  if (true) {
+    personName = 'Paul';
+  }
+
+  personName += ' plays football';
+
+  return personName;
+}
+
+let personName;
+
+canPlay();
+
+console.log(personName);
