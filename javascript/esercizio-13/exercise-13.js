e6afc9de6d74864f113cdd31adb10c0b608793bf
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-03-18 17:27:09 +0100

    feat/exercise 13 completed

--- javascript/esercizio-13/exercise-13.js
+++ javascript/esercizio-13/exercise-13.js
@@ -1,10 +1,10 @@
-const sum = (a, b) => `(${a} + ${b})`;
+const sum = (a, b) => a + b;
 
-const subtract = (a, b) => `${a} - ${b})`;
+const subtract = (a, b) => a - b;
 
-const multiply = (a, b) => `(${a} * ${b})`;
+const multiply = (a, b) => a * b;
 
-const divide = (a, b) => `${a} / ${b}`;
+const divide = (a, b) => a / b;
 
 const log = (value) => console.log(value);
 
