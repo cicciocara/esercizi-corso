2351566f88f43d459630940a27d324691da04c7f
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-03-18 16:41:07 +0100

    feat/exercise 38 completed

---
+++ javascript/esercizio-38/exercise-38.js
@@ -0,0 +1,47 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    this.#amount += amount;
+  }
+
+  withdraw(amount) {
+    this.#amount -= amount;
+  }
+
+  view() {
+    console.log(this.#amount);
+  }
+
+  getAmount() {
+    return this.#amount;
+  }
+
+  setAmount(amount) {
+    this.#amount += amount;
+  }
+}
+
+class BankAccountVip extends BankAccount {
+  constructor(initialAmount) {
+    super(initialAmount);
+  }
+
+  deposit(newamount) {
+    if (this.getAmount() >= 1000) {
+      newamount += (newamount * 3) / 100;
+      this.setAmount(newamount);
+    }
+  }
+}
+
+const bankAccountVip = new BankAccountVip(1000);
+bankAccountVip.deposit(500);
+bankAccountVip.deposit(1200);
+bankAccountVip.withdraw(800);
+bankAccountVip.deposit(3500);
+bankAccountVip.view();
