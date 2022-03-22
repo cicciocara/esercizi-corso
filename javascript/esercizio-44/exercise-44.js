b4c5ff3f1e4f1aae96f476eff978be51a70bf76f
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-03-22 11:25:11 +0100

    feat/exercise 44 completed

--- javascript/esercizio-44/exercise-44.js
+++ javascript/esercizio-44/exercise-44.js
@@ -3,6 +3,6 @@ let num2 = 20;
 console.log("Before swap: ", num1, num2); // Before swap: 10 20
 
 // Variable swapping
-[num1, num2] = [20, 10];
+[num1, num2] = [num2, num1];
 
 console.log("After swap: ", num1, num2); // After swap: 20 10
