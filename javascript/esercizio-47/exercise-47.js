6b6bf2226b31cf03a35e468a1dd48fbb738ce948
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-03-23 10:37:01 +0100

    feat/exercise 47 completed

--- javascript/esercizio-47/exercise-47.js
+++ javascript/esercizio-47/exercise-47.js
@@ -5,6 +5,10 @@ const person = {
   age: 25,
 };
 
-for (let item of Object.entries(person)) {
-  console.log(`${item[0]}: ${item[1]}`);
-}
+const { id, firstName, lastName, age } = person;
+console.log(
+  `id: ${id} 
+  firstName: ${firstName} 
+  lastName: ${lastName} 
+  age: ${age}`,
+);
